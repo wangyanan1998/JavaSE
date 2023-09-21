110235986b0da90da4810090220f804bb4d6992a
AD钙 <oncwnuHg-6wSl5Vpps3EXmJj3RtQ@git.weixin.qq.com>
2023-09-21 16:29:21 +0800

    feat:项目搭建

---
+++ src/main/java/com/wyn/javase/JavaSeApplication.java
@@ -0,0 +1,13 @@
+package com.wyn.javase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wyn/javase/JavaSeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wyn.javase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
